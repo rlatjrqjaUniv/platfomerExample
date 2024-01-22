248ab5d939ebd32c8462c246d217fc60cff9f23c
rlatjrqja <rlatjrqja@yuhan.ac.kr>
2024-01-22 15:13:15 +0900

    점프 커밋

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    enum State { Idling,Moving,Jumping,Falling }
+    [SerializeField] State state;
     
     public float moveCooltime; // 이동 키를 눌렀을때 소수점 단위로 힘을 가할건지(횟수), (moveSpedd * moveColltime = 가해지는 힘이라고 보면 됨)
     public float moveSpeed; // move Cooltime 1회에 얼만큼의 힘을 가할것인지(힘)
@@ -12,14 +15,23 @@ public class PlayerController : MonoBehaviour
     // ex) moveCooltime 0.1초, moveSpeed 110 이라면 0.1초동안 110의 힘이 다 소진되기 전에 0.1초가 지나고, 110의 힘이 또 추가되기 때문에 가속도O
     
     public float MaxSpeed; //Rigidbody에 info 보면 velocity 있음 너무 가속되지 않게 추가할 예정
+    public float jumpPower;
 
     public Rigidbody2D rb;
     private bool isMoving = false;
+    private bool isJuming = false;
+
+    public event EventHandler OnKeyPressed;
 
     private void Start()
     {
         // Rigidbody2D 컴포넌트 가져오기
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Mathf.Clamp(rb.velocity.x, -MaxSpeed, MaxSpeed);
     }
 
     public void MoveLeft()
@@ -38,11 +50,25 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void Jump()
+    {
+        if(!isJuming)
+        {
+            isJuming = true;
+            rb.AddForce(Vector2.up * jumpPower * 100);
+        }
+    }
+
     IEnumerator MoveCoroutine(Vector2 direction)
     {
         isMoving = true;
         rb.AddForce(direction * moveSpeed, ForceMode2D.Force);
         yield return new WaitForSeconds(moveCooltime); // 조절 가능한 딜레이 시간
         isMoving = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJuming = false;
     }
 }
